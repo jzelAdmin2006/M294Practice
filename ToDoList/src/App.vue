d059a791110283056d06b34cbd480d5500f5f469
jzelAdmin2006 <jo.zeller@protonmail.com>
2023-03-14 13:31:57 +0100

    solve task 4

---
+++ ToDoList/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue'
+  const todos = ref([
+    { task: "Vue.js lernen", done: true },
+    { task: "Franz lernen", done: true },
+    { task: "An Plänen zur Weltherrschaft arbeiten", done: false },
+    { task: "Katzenklo reinigen", done: false }
+  ])
+</script>
+
+<template>
+  <h1>Todo - Listen anzeigen</h1>
+  <div v-for="todo in todos">
+    <input type="checkbox" v-model="todo.done"/> {{ todo.task }} <span v-if="todo.done">🎉</span>
+  </div>
+</template>
+
+<style>
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+  }
+</style>
