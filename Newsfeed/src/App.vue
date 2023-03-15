808b1c7869e9d7d494de0bfc05276e1b4b23d448
jzelAdmin2006 <jo.zeller@protonmail.com>
2023-03-15 10:18:14 +0100

    solve task 5

---
+++ Newsfeed/src/App.vue
@@ -0,0 +1,60 @@
+<script setup>
+import { ref } from 'vue'
+import NewsfeedItem from './NewsfeedItem.vue'
+
+const items = ref([
+  {
+    id: 1,
+    title: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr',
+    teaser: 'At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+    numberOfWords: 512,
+    image: 'https://placekitten.com/200/200',
+  },
+  {
+    id: 2,
+    title: 'At vero eos et accusam et justo duo',
+    teaser: 'Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren.',
+    numberOfWords: 1723,
+    image: 'https://placekitten.com/200/200',
+  },
+  {
+    id: 3,
+    title: 'Consetetur sadipscing elitr, sed diam nonumy eirmod tempor ',
+    teaser: 'labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+    numberOfWords: 821,
+    image: 'https://placekitten.com/200/200',
+  },
+])
+</script>
+
+<template>
+  <h1>Newsfeed</h1>
+  <div class="box">
+    <div class="newsfeed">
+      <NewsfeedItem v-for="item in items" :key="item.id" :title="item.title" :teaser="item.teaser" :image="item.image"
+        :numberOfWords="item.numberOfWords" />
+    </div>
+  </div>
+</template>
+
+<style>
+body {
+  background: #0f172a;
+  color: #fff;
+  margin: 4rem auto;
+  max-width: 620px;
+  padding: 20px;
+}
+
+.box {
+  box-shadow: inset 0 1px 0 0 hsla(0, 0%, 100%, .05);
+  background: rgb(30 41 59/1);
+  border-radius: 5px;
+  overflow: hidden;
+}
+
+.newsfeed {
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ Newsfeed/src/NewsfeedItem.vue
@@ -0,0 +1,77 @@
+<script setup>
+defineProps({
+    title: {
+        type: String,
+        required: true
+    },
+    teaser: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    numberOfWords: {
+        type: Number,
+        required: true
+    }
+})
+</script>
+
+<template>
+    <div class="newsfeed__item">
+        <div class="newsfeed__image">
+            <img v-bind:src="image">
+        </div>
+        <div class=" newsfeed__text">
+            <h2 class="newsfeed__heading">
+                {{ title }}
+            </h2>
+            <p class="newsfeed__teaser">
+                {{ teaser }}
+            </p>
+            <p class="newsfeed__meta">
+                {{ Math.round(numberOfWords / 300) }} Minuten Lesezeit
+            </p>
+        </div>
+    </div>
+</template>
+
+<style>
+.newsfeed__image img {
+    display: block;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+}
+
+.newsfeed__text {
+    padding: 20px;
+}
+
+.newsfeed__item {
+    display: grid;
+    grid-template-columns: 200px auto;
+}
+
+.newsfeed__heading {
+    font-size: 18px;
+    margin: 10px 0;
+}
+
+.newsfeed__teaser {
+    font-size: 15px;
+    line-height: 1.25;
+}
+
+.newsfeed__meta {
+    font-size: 15px;
+    line-height: 1.25;
+    color: rgba(255, 255, 255, .6);
+}
+
+.newsfeed__item+.newsfeed__item {
+    border-top: 1px solid hsla(0, 0%, 100%, .05);
+}
+</style>
